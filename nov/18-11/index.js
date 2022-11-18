b032955c07f239338c959c9ad1e6a075cd7e814d
NAMRATA <namratajhait@gmail.com>
2022-11-18 17:38:38 +0530

    first commit

---
+++ nov/18-11/index.js
@@ -0,0 +1,7 @@
+function arrayfindvalue() {
+    let newArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    for(var i = 0; i< newArray.length; i++){
+        console.log(newArray[i])
+    }
+    }
+arrayfindvalue()
